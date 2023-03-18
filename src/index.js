17b21d8dd95ade0db30ef519c3a27ef10db68395
Taras <topanasenko85@gmail.com>
2023-03-19 00:24:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 2300;
 
 const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 searchBox.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
-
+const array = [];
+let countri = [];
 // fetchCountries(name);
 
 // ?fields={field},{field},{field}
@@ -17,6 +20,7 @@ searchBox.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
 
 function fetchCountries(name) {
   console.log(name.target.value);
+  countryList.innerHTML = '';
   count = name.target.value;
   fetch(
     `https://restcountries.com/v2/name/${count}?fields=name,capital,population,flags,languages`
@@ -24,30 +28,29 @@ function fetchCountries(name) {
     .then(response => {
       return response.json();
     })
-    .then(contries => {
-      console.log(contries);
-      elementList(contries);
-      // const array = [];
-      // const elementList = contries.map(function (value) {
-      //   const list = document.createElement('li');
-      //   list.classList.add('item');
-      //   list.textContent = value.name;
-      //   array.push(list);
-      // });
-      // countryList.append(...array);
+    .then(countries => {
+      if (!countries) {
+        console.log(countries.length);
+        console.log('000');
+      } else if (countries.length > 1) {
+        console.log(countries.length);
+        console.log('1++');
+        elementList(countries);
+      } else if (countries.length === 1) {
+        console.log(countries.length);
+        console.log(1);
+      }
     });
-}
-const array = [];
-function elementList(countries) {
-  if (countries.length > 0) {
-    countries.map(function (value) {
-      const list = document.createElement('li');
-      list.classList.add('item');
-      list.textContent = value.name;
-      array.push(list);
-    });
-  }
-  countryList.append(...array);
 }
 
-const countryList = document.querySelector('.country-list');
+function elementList(countries) {
+  countries.forEach(function (element) {
+    const list = `<li class="country-item"><img class="country-img" src="${element.flags.svg}" alt="flag" width=50px><h2 class="country-text">${element.name}</h2></li>`;
+    countri.push(list);
+  });
+
+  countryList.innerHTML = countri;
+
+  console.log(countri);
+  countri = [];
+}
