bcc8e1f04b267a850b661baac5f48dbc4e27b732
Taras <topanasenko85@gmail.com>
2023-03-18 16:32:03 +0200

    fetchCountries add

---
+++ src/fetchCountries.js
@@ -0,0 +1,5 @@
+fetchCountries(name);
+
+// ?fields={field},{field},{field}
+
+// https://restcountries.com/v2/name/{name}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+import debounce from 'lodash.debounce';
+
+const DEBOUNCE_DELAY = 2300;
+
+const searchBox = document.querySelector('#search-box');
+
+searchBox.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+
+// fetchCountries(name);
+
+// ?fields={field},{field},{field}
+// ?fields=capital,population,flags,languages,coatOfArms.svg
+
+// https://restcountries.com/v2/name/{name}
+
+function fetchCountries(name) {
+  console.log(name.target.value);
+  count = name.target.value;
+  fetch(
+    `https://restcountries.com/v2/name/${count}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(contries => {
+      console.log(contries);
+    });
+}
